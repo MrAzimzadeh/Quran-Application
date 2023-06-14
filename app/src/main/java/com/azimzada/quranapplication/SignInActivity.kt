b50b6bebdfb3153f8a7c414325e1c48ad5b81cc6
Karam K. Afandi <77574018+karamlyy@users.noreply.github.com>
2023-06-15 00:26:47 +0400

    empty

--- app/src/main/java/com/azimzada/quranapplication/SignInActivity.kt
+++ app/src/main/java/com/azimzada/quranapplication/SignInActivity.kt
@@ -4,15 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.azimzada.quranapplication.databinding.ActivitySignInBinding
+import com.azimzada.quranapplication.databinding.ActivityStartBinding
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding =  ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val getStartedButton = findViewById<Button>(R.id.signInButton)
-
-        getStartedButton.setOnClickListener {
+        binding.signInButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/azimzada/quranapplication/StartActivity.kt
+++ app/src/main/java/com/azimzada/quranapplication/StartActivity.kt
@@ -4,17 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.azimzada.quranapplication.databinding.ActivitySettingsBinding
+import com.azimzada.quranapplication.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        binding =  ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
-
-        getStartedButton.setOnClickListener {
+        binding.getStartedButton.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
     }
 }
+
